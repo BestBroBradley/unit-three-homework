dd225c34e24920e2d2791d88c5f4089337a6b830
Allison Bradley <allison.bradley@outlook.com>
2019-12-11 22:14:06 -0700

    Removed copy to clipboard button and tweaked CSS padding

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,57 +1,57 @@
 function createPassword() {
 
-var pwLength = prompt("How many characters would you like to use?\n\nEnter a number between 8 and 128.")
+    var pwLength = prompt("How many characters would you like to use?\n\nEnter a number between 8 and 128.")
 
-if (pwLength >= 8 && pwLength <= 128) {
-    var includeSpecial = confirm("Would you like to include special characters?")
-    var includeNum = confirm("Would you like to include numerals?")
-    var includeLower = confirm("Would you like to include lowercase letters?")
+    if (pwLength >= 8 && pwLength <= 128) {
+        var includeSpecial = confirm("Would you like to include special characters?")
+        var includeNum = confirm("Would you like to include numerals?")
+        var includeLower = confirm("Would you like to include lowercase letters?")
 
-    if (includeSpecial === true || includeNum === true || includeLower === true) {
-        var includeUpper = confirm("Would you like to include uppercase letters?")
-    } else {
-        alert("Your password will contain upper case letters only.")
-        var includeUpper = true;
-    }
+        if (includeSpecial === true || includeNum === true || includeLower === true) {
+            var includeUpper = confirm("Would you like to include uppercase letters?")
+        } else {
+            alert("Your password will contain upper case letters only.")
+            var includeUpper = true;
+        }
 
-    console.log(pwLength);
-    console.log(includeSpecial);
-    console.log(includeNum);
-    console.log(includeLower);
-    console.log(includeUpper);
+        console.log(pwLength);
+        console.log(includeSpecial);
+        console.log(includeNum);
+        console.log(includeLower);
+        console.log(includeUpper);
 
-    var special = ["!", "@", "#", "$", "%", "&", "*", "+", "="]
-    var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    
-    var charArray = [];
+        var special = ["!", "@", "#", "$", "%", "&", "*", "+", "="]
+        var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-function pushIf (includeType, type) {
-    if (includeType) {
-        charArray.push.apply(charArray, type)
-    }
-    }
+        var charArray = [];
 
-    pushIf (includeSpecial, special);
-    pushIf (includeNum, num);
-    pushIf (includeLower, lower);
-    pushIf (includeUpper, upper);
+        function pushIf(includeType, type) {
+            if (includeType) {
+                charArray.push.apply(charArray, type)
+            }
+        }
 
-    console.log(charArray);
-    
-    pwArray = [];
-    
-    for (var i = 0; i < pwLength; i++){
-        // console.log(charArray[Math.floor(Math.random() * charArray.length)]);
-        var char = charArray[Math.floor(Math.random() * charArray.length)];
-        pwArray.push(char);
-        
-    }
-    var finalPW = pwArray.join("");
-    // alert(finalPW);
-    document.getElementById("password-input").textContent = finalPW
+        pushIf(includeSpecial, special);
+        pushIf(includeNum, num);
+        pushIf(includeLower, lower);
+        pushIf(includeUpper, upper);
 
-} else alert("You must enter a number between 8 and 128.`")
+        console.log(charArray);
+
+        pwArray = [];
+
+        for (var i = 0; i < pwLength; i++) {
+            // console.log(charArray[Math.floor(Math.random() * charArray.length)]);
+            var char = charArray[Math.floor(Math.random() * charArray.length)];
+            pwArray.push(char);
+
+        }
+        var finalPW = pwArray.join("");
+        document.getElementById("passwordInput").textContent = finalPW
+
+    } else alert("You must enter a number between 8 and 128.`")
 
 }
+
